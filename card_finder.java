ae41d30aec150d954cf5285732a42ce24cbb4689
nathsdm <124789113+nathsdm@users.noreply.github.com>
2023-02-17 14:15:46 +0100

    Update card_finder.java

--- card_finder.java
+++ card_finder.java
@@ -1,22 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.utils.Converters;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class yu {
+public class Main {
     public static void main(String[] args) {
+        System.setProperty("java.library.path", "D:/Programmes/opencv/build/java/x64/opencv_java470.dll");
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat image = Imgcodecs.imread("tocrop2.jpg");
+        Mat image = Imgcodecs.imread("src/tocrop2.jpg");
         int scale_percent = 40;
-        int width = (int) (image.cols() * scale_percent / 100);
-        int height = (int) (image.rows() * scale_percent / 100);
+        int width = image.cols() * scale_percent / 100;
+        int height = image.rows() * scale_percent / 100;
         Size newSize = new Size(width, height);
         Imgproc.resize(image, image, newSize);
 
@@ -28,34 +26,28 @@ public class yu {
 
         Imgcodecs.imwrite("step1.jpg", edged);
 
-        List<MatOfPoint> cnts = new ArrayList<MatOfPoint>();
+        List<MatOfPoint> cnts = new ArrayList<>();
         Imgproc.findContours(edged.clone(), cnts, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-        Collections.sort(cnts, new Comparator<MatOfPoint>() {
-            public int compare(MatOfPoint o1, MatOfPoint o2) {
-                double area1 = Imgproc.contourArea(o1);
-                double area2 = Imgproc.contourArea(o2);
-                if (area1 > area2)
-                    return -1;
-                else if (area1 < area2)
-                    return 1;
-                else
-                    return 0;
-            }
+        cnts.sort((o1, o2) -> {
+            double area1 = Imgproc.contourArea(o1);
+            double area2 = Imgproc.contourArea(o2);
+            return Double.compare(area2, area1);
         });
 
         MatOfPoint screenCnt = new MatOfPoint();
-        for (int i = 0; i < cnts.size(); i++) {
+        for (MatOfPoint cnt : cnts) {
+            MatOfPoint2f  cnt2 = new MatOfPoint2f( cnt.toArray() );
             MatOfPoint2f approx = new MatOfPoint2f();
-            MatOfPoint2f contour2f = new MatOfPoint2f(cnts.get(i).toArray());
+            MatOfPoint2f contour2f = new MatOfPoint2f(cnt.toArray());
             double peri = Imgproc.arcLength(contour2f, true);
-            Imgproc.approxPolyDP(contour2f, approx, 0.02 * peri, true);
+            Imgproc.approxPolyDP(cnt2, approx, 0.02 * peri, true);
             if (approx.toArray().length == 4) {
-                screenCnt = cnts.get(i);
+                screenCnt = new MatOfPoint(approx.toArray());
                 break;
             }
         }
 
-        Imgproc.drawContours(image, Arrays.asList(screenCnt), -1, new Scalar(0, 0, 255), 3);
+        Imgproc.drawContours(image, List.of(screenCnt), -1, new Scalar(0, 0, 255), 3);
         Imgcodecs.imwrite("step2.jpg", image);
 
         Point[] pts = screenCnt.toArray();
@@ -70,11 +62,11 @@ public class yu {
         for (int i = 0; i < pts.length; i++) {
             diff[i] = pts[i].x - pts[i].y;
         }
-        rect[1] = pts[Utils.minIndex(diff)];
-        rect[3] = pts[Utils.maxIndex(diff)];
+        rect[3] = pts[Utils.minIndex(diff)];
+        rect[1] = pts[Utils.maxIndex(diff)];
 
-        double widthA = Math.sqrt(Math.pow(rect[2].x - rect[0].x, 2) + Math.pow(rect[2].y - rect[0].y, 2));
-        double widthB = Math.sqrt(Math.pow(rect[1].x - rect[3].x, 2) + Math.pow(rect[1].y - rect[3].y, 2));
+        double widthA = Math.sqrt(Math.pow(rect[2].x - rect[3].x, 2) + Math.pow(rect[2].y - rect[3].y, 2));
+        double widthB = Math.sqrt(Math.pow(rect[1].x - rect[0].x, 2) + Math.pow(rect[1].y - rect[0].y, 2));
         double maxWidth = Math.max(widthA, widthB);
         double heightA = Math.sqrt(Math.pow(rect[1].x - rect[2].x, 2) + Math.pow(rect[1].y - rect[2].y, 2));
         double heightB = Math.sqrt(Math.pow(rect[0].x - rect[3].x, 2) + Math.pow(rect[0].y - rect[3].y, 2));
@@ -82,14 +74,17 @@ public class yu {
 
         Point[] dst = new Point[4];
         dst[0] = new Point(0, 0);
-        dst[1] = new Point(maxWidth - 1, 0);
-        dst[2] = new Point(maxWidth - 1, maxHeight - 1);
-        dst[3] = new Point(0, maxHeight - 1);
+        dst[1] = new Point((maxWidth - 1)*2, 0);
+        dst[2] = new Point((maxWidth - 1)*2, (maxHeight - 1)*2);
+        dst[3] = new Point(0, (maxHeight - 1)*2);
 
         Mat M = Imgproc.getPerspectiveTransform(Converters.vector_Point2f_to_Mat(Arrays.asList(rect)), Converters.vector_Point2f_to_Mat(Arrays.asList(dst)));
         Mat warped = new Mat();
         Imgproc.warpPerspective(image, warped, M, new Size(maxWidth * 2, maxHeight * 2));
         Imgcodecs.imwrite("step3.jpg", warped);
+        Rect rectangle = new Rect((int) (4*maxWidth/3),(int) (7*maxHeight/5),(int) maxWidth/2,(int) maxHeight/10);
+        Mat image_roi = new Mat(warped,rectangle);
+        Imgcodecs.imwrite("step4.jpg", image_roi);
     }
 }
 
