f02dbc9ba4ea1f5d2601b0cecd29a1b48e0e3584
clement <clement.bourvic@telecom-sudparis.eu>
2023-05-10 11:13:54 +0000

    Added: Marche dans certaine condition pour le crop

--- CameraXApp/app/src/main/java/com/example/cameraxapp/MainActivity.kt
+++ CameraXApp/app/src/main/java/com/example/cameraxapp/MainActivity.kt
@@ -2,43 +2,101 @@ package com.example.cameraxapp
 
 
 
-import org.opencv.android.Utils
-import org.opencv.core.CvType
-import org.opencv.core.Mat
 import android.Manifest
 import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.ColorSpace
+import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.cameraxapp.databinding.ActivityMainBinding
+import org.opencv.android.Utils
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
+import java.io.File
+import java.io.FileOutputStream
 import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-fun uriToMat(contentResolver: ContentResolver, uri: Uri): Mat {
-    val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
-    val mat = Mat(bitmap.height, bitmap.width, CvType.CV_64F)
-    Utils.bitmapToMat(bitmap, mat)
-    return mat
+
+//debug
+fun saveDebugImageToMediaStore(context: Context,debugImageName: String, mat: Mat): Uri? {
+    // Create a ContentValues object to store metadata about the image
+    val values = ContentValues().apply {
+        put(MediaStore.Images.Media.DISPLAY_NAME, "${debugImageName}_${System.currentTimeMillis()}.png")
+        put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/DebugImages")
+    }
+
+    // Get the content resolver and insert the new image into the MediaStore
+    val resolver = context.contentResolver
+    val uri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+    // Open an output stream to write the image data to the MediaStore
+    uri?.let {
+        val outputStream: OutputStream = resolver.openOutputStream(uri) ?: return null
+
+        // Convert the Mat to a Bitmap and write it to the output stream
+        val bitmap = matToBitmap(mat)
+
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+
+        outputStream.flush()
+        outputStream.close()
+    }
+
+    return uri
 }
+//mat to bitmap
+fun matToBitmap(mat: Mat): Bitmap {
+    val rgbaMat = Mat(mat.rows(), mat.cols(), CvType.CV_8UC4)
+    Imgproc.cvtColor(mat, rgbaMat, Imgproc.COLOR_BGR2RGBA)
+
+    val bitmap = Bitmap.createBitmap(rgbaMat.cols(), rgbaMat.rows(), Bitmap.Config.ARGB_8888)
+    Utils.matToBitmap(rgbaMat, bitmap)
+    return bitmap
+}
+//Transform an uri to a Mat
+fun uriToMat(contentResolver: ContentResolver, uri: Uri): Mat {
+    val source: ImageDecoder.Source = ImageDecoder.createSource(contentResolver, uri)
+    val bitmap = ImageDecoder.decodeBitmap(source) { decoder, _, _ ->
+        decoder.setTargetColorSpace(ColorSpace.get(ColorSpace.Named.SRGB))
+        decoder.allocator = ImageDecoder.ALLOCATOR_SOFTWARE
+    }
+
+    val rgbaMat = Mat(bitmap.height, bitmap.width, CvType.CV_8UC4)
+    Utils.bitmapToMat(bitmap, rgbaMat)
+
+    val bgrMat = Mat(rgbaMat.rows(), rgbaMat.cols(), CvType.CV_8UC3)
+    Imgproc.cvtColor(rgbaMat, bgrMat, Imgproc.COLOR_RGBA2BGR)
+
+    return bgrMat
+}
+
 // Function to save a Mat as a new image file in the MediaStore
 fun saveMatToMediaStore(context: Context, mat: Mat): Uri? {
     // Create a ContentValues object to store metadata about the image
     val values = ContentValues().apply {
-        put(MediaStore.Images.Media.DISPLAY_NAME, "theimage") // Set the display name for the image
+        put(MediaStore.Images.Media.DISPLAY_NAME, "IMG_${System.currentTimeMillis()}.jpg") // Set the display name for the image
         put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg") // Set the MIME type of the image
         put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image") // Set the relative path for the image
     }
@@ -52,8 +110,8 @@ fun saveMatToMediaStore(context: Context, mat: Mat): Uri? {
         val outputStream: OutputStream = resolver.openOutputStream(uri) ?: return null
 
         // Convert the Mat to a Bitmap and write it to the output stream
-        val bitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888)
-        Utils.matToBitmap(mat, bitmap)
+        val bitmap = matToBitmap(mat)
+
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
 
         outputStream.flush()
@@ -142,9 +200,18 @@ class MainActivity : AppCompatActivity() {
                 override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
                     // Convertir la photo en un objet Mat puis la découper et la sauvgarder dans la gallerie
                     val mat = uriToMat(contentResolver, outputFileResults.savedUri!!)
-                    val matcrop = Card_Finder.card(mat)
+                    val matcrop = Card_Finder.Card(mat,true,this@MainActivity)
+                    matcrop?.let {
+                        // Perform operations with the non-null matCrop value
+                    } ?: run {
+                        Log.e("YourTag", "No contours found.")
+                    }
                     val uri = matcrop?.let { saveMatToMediaStore(this@MainActivity, it) }
                     Log.d(TAG, "Photo capture succeeded: $uri")
+
+                    // Afficher un message lorsque la photo est enregistrée
+                    val message = "Photo enregistrée: $uri"
+                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
 
                 }
             }
